e817108f570207f5c470c4186e336e548dc450ec
Mario Rubio <marioastur@gmail.com>
2022-06-28 14:49:02 +0200

    Mostrar tarjetas em página inicial

--- marvelApi/src/components/TheWelcome.vue
+++ marvelApi/src/components/TheWelcome.vue
@@ -1,8 +1,16 @@
 <script>
-
-
+import { mapState } from "pinia";
+import { useMarvelStore } from "../stores/marvel.js";
+export default {
+  computed: {
+   ...mapState( useMarvelStore,['myMarvelData'] )
+  }
+};
 </script>
 
 <template>
-  <h1>ESTO ES EL INICIO</h1>
+  <div v-for="data in myMarvelData" :key=data.name>
+    <img v-bind:src="data.images.sm" alt="">
+    <p>{{ data.name }} / {{ data.alias[0] }} / {{ data.powerstats.combat }}</p>
+  </div>
 </template>
--- marvelApi/src/stores/marvel.js
+++ marvelApi/src/stores/marvel.js
@@ -1,40 +1,51 @@
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia'
 
-export const useMarvelStore = defineStore({
-  id: "counter",
-  state: () => ({
-    counter: 0,
-  }),
-  getters: {
-    doubleCount: (state) => state.counter * 2,
-  },
-  actions: {
-    marvelApi() {
-        const marvelApi = async() => {
-            try{
-                const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json")
-                const marvelJson = await response.json() ;        
-                marvelJson.map(element=>{
-                    if (element.biography.publisher == "Marvel Comics") {
-                        myMarvel.push(
-                            {name:element.name,
-                            powerstats:{
-                            intelligence:element.powerstats.intelligence,
-                            strength:element.powerstats.strength,
-                            speed:element.powerstats.speed,
-                            durability:element.powerstats.durability,
-                            power:element.powerstats.power,
-                            combat:element.powerstats.combat}
-                                }
-                            )
-                        }
-                })
-                return myMarvel
-            }
-            catch(error){
-                console.log('Error : ' + error)
-            }
+let myMarvel = new Array
+const marvelData = async() => {
+  try{
+    const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json")
+    const marvelJson = await response.json()
+    marvelJson.map(element=>{
+      if (element.biography.publisher == "Marvel Comics") {
+        myMarvel.push(
+            {name:element.name,
+              alias:element.biography.aliases,
+              values:0,
+              favorites:false,
+            powerstats:{
+            intelligence:element.powerstats.intelligence,
+            strength:element.powerstats.strength,
+            speed:element.powerstats.speed,
+            durability:element.powerstats.durability,
+            power:element.powerstats.power,
+            combat:element.powerstats.combat},
+            images:{
+              sm:element.images.sm,
+              md:element.images.md}
+                }
+            )
         }
-    },
+    })
+  }catch(error){
+  console.log('Error : ' + error)
+  }
+}
+
+marvelData()
+
+export const useMarvelStore = defineStore({  // suele usarse use+NOMBRE+Store
+  id: 'marvelApi',
+  state:()=>({
+    myMarvelData: myMarvel,
+  }),
+
+  getters: {
+    /*
+    doubleCount: (state) => state.myMarvel * 2,
+    */
   },
-});
+  
+  actions: {
+    
+  }
+})
