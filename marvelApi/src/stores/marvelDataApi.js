f33322a376e75343776a997df6325679f513a33d
Mario Rubio <marioastur@gmail.com>
2022-06-30 11:57:30 +0200

    Inicializa rama. Captura de datos API

---
+++ marvelApi/src/stores/marvelDataApi.js
@@ -0,0 +1,31 @@
+let myMarvel = new Array
+const marvelData = async() => {
+  try{
+    const response = await fetch("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json")
+    const marvelJson = await response.json()
+    marvelJson.map(element=>{
+      if (element.biography.publisher == "Marvel Comics") {
+        myMarvel.push(
+            {name:element.name,
+              id:element.id,
+              alias:element.biography.aliases,
+              values:0,
+              favorites:false,
+            powerstats:{
+            intelligence:element.powerstats.intelligence,
+            strength:element.powerstats.strength,
+            speed:element.powerstats.speed,
+            durability:element.powerstats.durability,
+            power:element.powerstats.power,
+            combat:element.powerstats.combat},
+            images:{
+              sm:element.images.sm,
+              md:element.images.md}
+                }
+            )
+        }
+    })
+  }catch(error){
+  console.log('Error : ' + error)
+  }
+}
