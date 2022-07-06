8eabdf25af7f693808d7c56172c7b783d9dbb312
sudoBuda <juanfranciscobalseca@gmail.com>
2022-07-06 23:16:51 +0200

    add heroe to favorite section

--- marvelApi/src/stores/marvel.js
+++ marvelApi/src/stores/marvel.js
@@ -1,18 +1,16 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useMarvelStore = defineStore({ 
-  id: 'marvelFavoritesArray',
-  state:()=>({
-    favoritesArray:[]
-  }),
+export const useMarvelStore = defineStore({
+	id: "marvelFavoritesArray",
+	state: () => ({
+		favoritesArray: [],
+	}),
 
-  getters: {
-  },
-  
-  actions: {
-    addFavorite(heroe)  {
-      this.favoritesArray.push(heroe)
-      console.log(this.favoritesArray)
-    }
-  }
-})
+	getters: {},
+
+	actions: {
+		addFavorite(heroe) {
+			this.favoritesArray.push(heroe);
+		},
+	},
+});
